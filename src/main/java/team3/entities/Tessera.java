998cb7ba64054a14f1c44c050a60d1df6eaf65f9
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-01-26 17:24:50 +0100

    creates Tessera and Utente entities

--- src/main/java/team3/entities/Tessera.java
+++ src/main/java/team3/entities/Tessera.java
@@ -1,4 +1,81 @@
 package team3.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tessere")
 public class Tessera {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "codice_tessera", unique = true, nullable = false)
+    private String codiceTessera;
+
+    @Column(name = "data_emissione")
+    private LocalDate dataEmissione;
+
+    @Column(name = "data_scadenza")
+    private LocalDate dataScadenza;
+
+    @OneToOne
+    @JoinColumn(
+            name = "utente_id",
+            nullable = false,
+            unique = true
+    )
+    private Utente utente;
+
+    // Costruttori
+    public Tessera() {
+    }
+
+    public Tessera(String codiceTessera, LocalDate dataEmissione, LocalDate dataScadenza, Utente utente) {
+        this.codiceTessera = codiceTessera;
+        this.dataEmissione = dataEmissione;
+        this.dataScadenza = dataScadenza;
+        this.utente = utente;
+    }
+
+    // Getter e Setter
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getCodiceTessera() {
+        return codiceTessera;
+    }
+
+    public void setCodiceTessera(String codiceTessera) {
+        this.codiceTessera = codiceTessera;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public LocalDate getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(LocalDate dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
 }
