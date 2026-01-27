61c8ada8f3f2ba17be17f5eca369edfa5780e79a
virgilistrate <virgilistrate24@gmail.com>
2026-01-27 15:27:20 +0100

    adds percorrenzaDAO

--- src/main/java/team3/dao/PercorrenzaDAO.java
+++ src/main/java/team3/dao/PercorrenzaDAO.java
@@ -1,4 +1,136 @@
 package team3.dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+import team3.entities.MezzoDiTrasporto;
+import team3.entities.Percorrenza;
+import team3.entities.Tratta;
+import team3.exceptions.NotFoundException;
+
+import java.util.List;
+import java.util.UUID;
+
 public class PercorrenzaDAO {
+    private EntityManager em;
+
+    public PercorrenzaDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    // 1) SALVO UNA PERCORRENZA
+    public void save(Percorrenza newPercorrenza) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        em.persist(newPercorrenza);
+
+        transaction.commit();
+        System.out.println("La percorrenza " + newPercorrenza.getId_percorrenze() + " è stata salvata correttamente!");
+    }
+
+    // 2) TRATTA: CREO E SALVO TRATTA
+    public void saveTratta(Tratta newTratta) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        em.persist(newTratta);
+
+        transaction.commit();
+        System.out.println("La tratta " + newTratta.getId_tratta() + " è stata salvata correttamente!");
+    }
+
+    // 3)  CERCO TRATTA PER ID
+    public Tratta findTrattaById(String trattaId) {
+        Tratta found = em.find(Tratta.class, UUID.fromString(trattaId));
+        if (found == null) throw new NotFoundException(trattaId);
+        return found;
+    }
+
+    // 4) CREA E SALVA PERCORRENZA COLLEGANDO UNA TRATTA GIÀ ESISTENTE
+
+    public void createAndSave(MezzoDiTrasporto mezzo, String trattaId, int minutiEffettivi) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Tratta tratta = em.find(Tratta.class, UUID.fromString(trattaId));
+        if (tratta == null) {
+            transaction.rollback();
+            throw new NotFoundException("Tratta non trovata: " + trattaId);
+        }
+
+        Percorrenza p = new Percorrenza(mezzo, tratta, minutiEffettivi);
+        em.persist(p);
+
+        transaction.commit();
+
+        System.out.println("Percorrenza creata correttamente! tratta_id=" + trattaId);
+    }
+
+    // 5) CREA TUTTO INSIEME: CREA TRATTA + CREA PERCORRENZA
+
+    public void createTrattaAndPercorrenza(MezzoDiTrasporto mezzo,
+                                           String zonaPartenza,
+                                           String capolinea,
+                                           int tempoPrevistoMinuti,
+                                           int minutiEffettivi) {
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Tratta tratta = new Tratta(zonaPartenza, capolinea, tempoPrevistoMinuti);
+        em.persist(tratta);
+
+        Percorrenza p = new Percorrenza(mezzo, tratta, minutiEffettivi);
+        em.persist(p);
+
+        transaction.commit();
+
+        System.out.println("Tratta + Percorrenza create correttamente! tratta_id=" + tratta.getId_tratta());
+    }
+
+    // 6) PERCORRENZA: RICERCA PER ID
+    public Percorrenza findById(String percorrenzaId) {
+        Percorrenza found = em.find(Percorrenza.class, UUID.fromString(percorrenzaId));
+        if (found == null) throw new NotFoundException(percorrenzaId);
+        return found;
+    }
+
+    // 7) PERCORRENZA: TROVO TUTTE
+    public List<Percorrenza> findAllPercorrenze() {
+        TypedQuery<Percorrenza> query = em.createQuery("SELECT p FROM Percorrenza p", Percorrenza.class);
+        return query.getResultList();
+    }
+
+    // 8) ADMIN : TEMPO MEDIO EFFETTIVO su tratta+mezzo
+    public double getTempoMedioEffettivo(MezzoDiTrasporto mezzo, String trattaId) {
+        Tratta tratta = findTrattaById(trattaId);
+
+        TypedQuery<Double> query = em.createQuery(
+                "SELECT AVG(p.minuti_effettivi) FROM Percorrenza p WHERE p.mezzo = :mezzo AND p.tratta = :tratta",
+                Double.class
+        );
+        query.setParameter("mezzo", mezzo);
+        query.setParameter("tratta", tratta);
+
+        Double res = query.getSingleResult();
+        return res == null ? 0.0 : res;
+    }
+
+    // 9) DELETE
+    public void findByIdAndDelete(String percorrenzaId) {
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Percorrenza found = em.find(Percorrenza.class, UUID.fromString(percorrenzaId));
+        if (found == null) {
+            transaction.rollback();
+            throw new NotFoundException(percorrenzaId);
+        }
+
+        em.remove(found);
+
+        transaction.commit();
+        System.out.println("La percorrenza " + percorrenzaId + " è stata cancellata correttamente!");
+    }
 }
