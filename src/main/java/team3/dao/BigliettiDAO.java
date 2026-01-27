9b90f41fab4565067c52473abd6e435476d70246
Hammers <gianluca.hammers@gmail.com>
2026-01-27 15:59:40 +0100

    g

--- src/main/java/team3/dao/BigliettiDAO.java
+++ src/main/java/team3/dao/BigliettiDAO.java
@@ -23,5 +23,55 @@ public class BigliettiDAO {
             LocalDate dataEmissione
     ) {
         EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
 
+            List result = em.createNativeQuery(
+                                               """
+                                               SELECT *
+                                               FROM biglietto
+                                                 WHERE data_emissione = :dataEmissione
+                                                 AND data_vidimazione IS NOT NULL
+                                               """,
+                                               Biglietto.class
+                                       )
+                                       .setParameter("mezzoId", mezzoId)
+                                       .setParameter("dataEmissione", dataEmissione)
+                                       .getResultList();
+
+            et.commit();
+            return result;
+
+        } catch (PersistenceException e) {
+            if (et.isActive()) et.rollback();
+            throw new RuntimeException("Errore nel recupero dei biglietti vidimati per mezzo", e);
+        }
+    }
+    public long countBigliettiVidimatiPeriodo(
+            LocalDateTime from,
+            LocalDateTime to
+    ) {
+        EntityTransaction et = em.getTransaction();
+        try {
+            et.begin();
+
+            Number count = (Number) em.createNativeQuery(
+                                              """
+                                              SELECT COUNT(*)
+                                              FROM biglietti
+                                              WHERE data_vidimazione BETWEEN :from AND :to
+                                              """
+                                      )
+                                      .setParameter("from", from)
+                                      .setParameter("to", to)
+                                      .getSingleResult();
+
+            et.commit();
+            return count.longValue();
+
+        } catch (PersistenceException e) {
+            if (et.isActive()) et.rollback();
+            throw new RuntimeException("errore nel conteggio dei biglietti vidimati nel periodo", e);
+        }
+    }
 }
