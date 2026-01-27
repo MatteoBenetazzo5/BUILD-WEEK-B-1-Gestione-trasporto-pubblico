2b7e959c4effa52f5a3392f75bd804704f1b5c78
Sabatino Provenza <sabatinoprovenza98@outlook.it>
2026-01-27 15:58:39 +0100

    adds PuntoVendita,Utente,Tessera DAO

--- src/main/java/team3/Application.java
+++ src/main/java/team3/Application.java
@@ -1,13 +1,12 @@
 package team3;
-
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporto_pubblicopu");
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 
 }
---
+++ src/main/java/team3/dao/PuntoVenditaDAO.java
@@ -0,0 +1,51 @@
+package team3.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import team3.entities.PuntoVendita;
+
+import java.util.UUID;
+
+public class PuntoVenditaDAO {
+    private final EntityManager entityManager;
+
+    public PuntoVenditaDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // --------- SAVE ---------
+    public void savePuntoVendita(PuntoVendita p) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+        entityManager.persist(p);
+        transaction.commit();
+    }
+
+    // --------- FIND BY ID  ---------
+    public PuntoVendita findById(UUID id) {
+
+        PuntoVendita p = entityManager.find(PuntoVendita.class, id);
+
+        if (p == null) {
+            System.out.println("Il punto vendita con id: " + id + " non è stato trovato");
+            return null;
+        }
+
+        return p;
+    }
+
+
+    // -------- DELETE --------
+    public boolean deleteById(UUID puntoVenditaId) {
+
+        PuntoVendita p = findById(puntoVenditaId);
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        entityManager.remove(p);
+        tx.commit();
+
+        return true;
+    }
+
+}
